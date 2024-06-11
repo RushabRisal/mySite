1284ee898256c0dd8242954d29f5ea63bc02502e
RushabRisal <rushabofficials@gmail.com>
2024-06-11 18:20:23 +0545

    landing page

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 const Logo = () => {
     return ( 
-        <h1 className="absolute top-0 left-0 p-2 md:p-5 font-serif font-bold text-lg z-10 flex justify-center items-center underline text-slate-200/50">
+        <h1 className="absolute top-0 left-0 p-2 md:p-5 font-serif font-bold text-lg z-10 flex justify-center items-center underline text-slate-100/85 ">
             Rushab
         </h1>
      );
